9e7bd5c51b79d0dca411152a91ffe75abeadcc87
Wisdom Wolf <WisdomWolf@users.noreply.github.com>
2020-07-19 23:31:23 -0400

    Fixes stupidity

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import wise_utils
 
 setup(name='python-wise-utils',
-      version=wise_untils.__version__,
+      version=wise_utils.__version__,
       py_modules=['wise_utils'],
       author='@wisdomwolf',
       author_email='wisdomwolf@gmail.com',
